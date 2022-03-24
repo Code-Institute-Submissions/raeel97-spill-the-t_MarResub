ad57f22b46b83985cf269deafb3d9eb3574d3451
raeel97 <hecate101@gmail.com>
2022-03-24 22:43:04 +0000

    set account email verification to none

--- spillthet/settings.py
+++ spillthet/settings.py
@@ -178,3 +178,6 @@ if 'USE_AWS' in os.environ:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ACCOUNT_EMAIL_VERIFICATION = "none"
